cc672202db6c649998c87563b609eedbfabf193f
Ian Gwillim <igwillim@gmail.com>
2018-11-18 01:09:09 -0500

    Parse data functionality added. Other improvments. Added Data class to handle parsing data. Currently pulls out data I find most usefull, but could be setup to output each value to a file. Current files: - Prestige - Level - TotalKills - TotalWins - EikaRatio - KDRatio - WinLoseRatio

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request = require('request');
 const credentials = require('./credentials');
+const Data = require('./scripts/Data');
+
 
 const options = {
-  url: 'https://cod-api.tracker.gg/v1/standard/bo4/profile/2/FIanker_',
+  url: `https://cod-api.tracker.gg/v1/standard/bo4/profile/2/${credentials.gamertag}`,
   headers: {
     'TRN-Api-Key': credentials.apiKey
   }
@@ -10,18 +12,11 @@ const options = {
 
 function getData(error, response, body) {
   if(error) console.log(`Err: ${error}`);
-  // console.log(`Status Code: ${response.statusCode}`);
 
-  let stats = JSON.parse(body);
-  
-  // console.log(stats.data.stats); // Full API data
+  // let stats = JSON.parse(body);
+  // console.log(stats.data.stats); // Full API stat data
 
-  for(let i = 0; i < stats.data.stats.length; i++) {
-    if(stats.data.stats[i].metadata.key === 'EkiaDeathRatio') {
-      console.log(stats.data.stats[i]);
-      console.log(`i = ${i}`);
-    }
-  }
+  Data.handleData(body);
 
 }
 
---
+++ scripts/Data.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+
+// Relevant data from the api that we want to use
+// let stats = {
+//   prestige: {title: 'Prestige', value: 0},
+//   level: {title: 'Level', value: 0},
+//   totalKills: {title: 'TotalKills', value: 0},
+//   totalWins: {title: 'TotalWins', value: 0},
+//   eikaRatio: {title: 'EikaRatio', value: 0},
+//   kdRatio: {title: 'KDRatio', value: 0}, //Actual kills ratio
+//   winLoseRatio: {title: 'WinLoseRatio', value: 0} // I believe, ratio is percent of games won (ex. 78%)
+// }
+let stats = [
+  {title: 'Prestige', value: 0},
+  {title: 'Level', value: 0},
+  {title: 'TotalKills', value: 0},
+  {title: 'TotalWins', value: 0},
+  {title: 'EikaRatio', value: 0},
+  {title: 'KDRatio', value: 0}, //Actual kills ratio
+  {title: 'WinLoseRatio', value: 0} // I believe, ratio is percent of games won (ex. 78%)
+]
+
+
+let handleData = apiData => {
+  let parsed = JSON.parse(apiData);
+  // console.log(parsed.data.stats); // Data less metadat. ie platform, user etc
+
+  // Used to get data keys and indexes
+  // for(let i = 0; i < parsed.data.stats.length; i++) {
+  //   let reference = [];
+  //   reference[i] = {key: parsed.data.stats[i].metadata.key, index: i};
+  //   console.log(`Array Key: ${parsed.data.stats[i].metadata.key}, Position: ${i}`);
+  //   // writeFiles('ReferenceFile', 'JSON', JSON.stringify(reference));
+  // }
+
+  let winLose = parsed.data.stats[16].value / parsed.data.stats[17].value;
+  // Fill stats obj
+  // stats.prestige.value = parsed.data.stats[0].displayValue;
+  // stats.level.value = parsed.data.stats[1].displayValue;
+  // stats.totalKills.value = parsed.data.stats[3].displayValue;
+  // stats.totalWins.value = parsed.data.stats[16].displayValue;
+  // stats.eikaRatio.value = parsed.data.stats[41].displayValue;
+  // stats.kdRatio.value = parsed.data.stats[2].displayValue;
+  // stats.winLoseRatio.value = winLose; // Converted (ex. 3.41 wins for ever lose)
+
+  stats[0].value = parsed.data.stats[0].displayValue;
+  stats[1].value = parsed.data.stats[1].displayValue;
+  stats[2].value = parsed.data.stats[3].displayValue;
+  stats[3].value = parsed.data.stats[16].displayValue;
+  stats[4].value = parsed.data.stats[41].displayValue;
+  stats[5].value = parsed.data.stats[2].displayValue;
+  stats[6].value = winLose; // Converted (ex. 3.41 wins for ever lose)
+  
+  for(let i = 0; i < stats.length; i++) {
+    writeFiles(stats[i].title, 'txt', stats[i].value);
+  }
+
+}
+
+let writeFiles = (fileName, fileType, data) => {
+  fs.writeFile(`./Data/${fileName}.${fileType}`, data, error => {
+    if(error) console.log(error);
+
+    console.log('FILE SAVED');
+  });
+}
+
+module.exports.handleData = handleData;
